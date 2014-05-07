7cb7e6a16849e9279be84f1da6610291716b27e6
TOSHIBA <TOSHIBA@TOSHIBA-PC.gateway.2wire.net>
2014-05-07 17:39:36 +0000

    correction des classe ecocyclefacade et ecocyclefacadelocal4

--- src/main/java/com/mycompany/ecolexpert/ejb/EcoCycleFacade.java
+++ src/main/java/com/mycompany/ecolexpert/ejb/EcoCycleFacade.java
@@ -30,10 +30,11 @@ public class EcoCycleFacade extends AbstractFacade<EcoCycle> implements EcoCycle
     public EcoCycleFacade() {
         super(EcoCycle.class);
     }
-    
+
+
     @Override
-    public List<EcoCycle> findAllDistincts(){  
-        String texteRequete = "SELECT DISTINCT c.codecycle, c.code3 FROM EcoCycle c";
+    public List<EcoCycle> findAllDistincts() {
+        String texteRequete = "SELECT c FROM EcoCycle c";
         Query nQuery = em.createQuery(texteRequete);
         return nQuery.getResultList(); 
     }
