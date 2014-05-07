dd06bbdc57703da917216962978c273bfa372cf7
TOSHIBA <TOSHIBA@TOSHIBA-PC>
2014-05-07 15:41:27 +0000

    correction des classe ecocyclefacade et ecocyclefacadelocal3

--- src/main/java/com/mycompany/ecolexpert/jpa/EcoCycle.java
+++ src/main/java/com/mycompany/ecolexpert/jpa/EcoCycle.java
@@ -34,7 +34,7 @@ import javax.xml.bind.annotation.XmlRootElement;
     @NamedQuery(name = "EcoCycle.findByCodecycle", query = "SELECT c FROM EcoCycle c WHERE c.codecycle = :codecycle"),
     @NamedQuery(name = "EcoCycle.findByCode3", query = "SELECT c FROM EcoCycle c WHERE c.code3 = :code3"),
     @NamedQuery(name = "EcoCycle.findByCodefiliere", query = "SELECT c FROM EcoCycle c WHERE c.codefiliere = :codefiliere"),
-    @NamedQuery(name = "EcoCycle.findAllDistinct", query = "SELECT c FROM EcoCycle c")})
+    @NamedQuery(name = "EcoCycle.findAllDistinct", query = "SELECT c.codecycle, c.code3 FROM EcoCycle c")})
 public class EcoCycle implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
